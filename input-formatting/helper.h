ce5cdcc741d1dbf0547c86dcfed24007fe21ea5b
Nicula Ionut <niculaionut@tutanota.com>
2021-04-28 23:27:06 +0300

    remove unused functions

--- input-formatting/helper.h
+++ input-formatting/helper.h
@@ -1,54 +1,6 @@
 #pragma once
 #include <vector>
-#include <string>
 #include <string_view>
-#include <algorithm>
-
-inline void ltrim(std::string& str)
-{
-        str.erase(str.begin(), std::find_if(str.begin(), str.end(),
-                                            [](char c)
-                                            {
-                                                    return !std::isspace(c);
-                                            }));
-}
-
-inline void rtrim(std::string& str)
-{
-        str.erase(std::find_if(str.rbegin(), str.rend(),
-                               [](char c)
-                               {
-                                       return !std::isspace(c);
-                               })
-                      .base(),
-                  str.end());
-}
-
-inline void trim(std::string& str)
-{
-        ltrim(str);
-        rtrim(str);
-}
-
-template<typename Container>
-inline std::enable_if_t<std::is_same_v<typename Container::value_type, std::string>>
-trim(Container& c)
-{
-        for(auto& str : c)
-        {
-                trim(str);
-        }
-}
-
-inline bool strcontains(std::string_view str, std::string_view target)
-{
-        return str.find(target) != std::string_view::npos;
-}
-
-inline bool strcontains(std::string_view str, char target)
-{
-        return str.find(target) != std::string_view::npos;
-}
 
 inline std::vector<std::string_view> split(const std::string_view s, const char delim = ' ')
 {
