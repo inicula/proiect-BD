274f872fb55325df22acdd29a2ca05e3fa133f01
Nicula Ionut <niculaionut@tutanota.com>
2021-05-12 22:29:55 +0300

    only 1 query with outer join is needed

--- queries/21.sql
+++ queries/21.sql
@@ -1,12 +1,15 @@
 --Pentru fiecare artist, afiseaza numele si prenumele sau si
---informatii despre albumele la care a contribuit.Coloanele
---aferente artistilor care nu au asociat niciun album
+--informatii despre track din albume la care a contribuit.
+--Coloanele aferente artistilor care nu au asociat niciun album
 --vor aparea cu valori 'null'.
 
 select art.first_name,
        art.last_name,
        group.name,
-       a.*
+       a.title,
+       a.release_date,
+       t.title,
+       t.length
 from artists art
 left join group_members g_m
       on art.id = g_m.member_id
@@ -14,16 +17,7 @@ left join groups g
       on g_m.group_id = g.id
 left join albums a
      on g.id = a.group_id
+left join tracks t
+     on a.id = t.album_id
 where g_m.left_group_date is null or
       g_m.left_group_date > a.release_date;
-
---Pentru fiecare angajat, sa se afiseze informatii despre fiecare
---abonament pe care l-a creat. Coloanele aferente angajatilor care
---nu au creat niciun abonament vor aparea cu valori 'null'.
-
-select e.employee_id,
-       e.last_name,
-       s.*
-from employees e
-left join subscriptions s
-     on e.employee_id = s.employee_id;
